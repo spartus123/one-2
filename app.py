9bcb8d08c140e1db8b52657dd06b19ddb14c9eb6
spartus123 <134217944+spartus123@users.noreply.github.com>
2023-05-28 16:59:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,28 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+PRODUKTAI: List[Dict[str, Union[str, int]]] = [
+    {
+        'id': 1,
+        'pavadinimas': "Logitech pele",
+        'aprasymas': "bla bla",
+        'kaina': 300,
+    },
+    {
+        'id': 2,
+        'pavadinimas': "Trust klaviatura",
+        'aprasymas': "tuoj bus",
+        'kaina': 400,
+    }
+]
 
-
-
-
-@app.route("/Apie mus")
+@app.route("/")
 def hello_world():
-    return render_template('apie_mus.html')
+    return render_template('home.html', produktai=PRODUKTAI)
 
-@app.route("/Kontaktai")
-def hello_world():
-    return render_template('kontaktai.html')
-
-@app.route("/Naujienos")
-def hello_world():
-    return render_template('naujienos.html')
-
-@app.route("/Produktai")
-def hello_world():
-    return render_template('produktai.html')
-
-
+@app.route("/produktai")
+def list_produktai():
+    return jsonify(PRODUKTAI)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
